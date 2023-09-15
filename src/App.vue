b685c40691effca22feb8edfd1a3c6fb7d48e783
Ericdid <ericdidom@gmail.com>
2023-09-15 11:44:23 +0200

    add.components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
-<script></script>
+<script>
+import appHeader from "./components/appHeader.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+
+  components: {
+    appHeader,
+  },
+};
+</script>
 
 <template></template>
 
-<style></style>
+<style lang="scss" scoped></style>
