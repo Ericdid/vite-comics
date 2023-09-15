30783628d6ba7734221cf1d2c9c1d5660c975fbd
Ericdid <ericdidom@gmail.com>
2023-09-15 12:09:16 +0200

    add.bootstrap/header

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -9,23 +9,40 @@ export default {
 </script>
 
 <template>
-  <img src="../assets/img/dc-logo.png" alt="" />
-  <ul class="d-flex">
-    <li>characters</li>
-    <li>comics</li>
-    <li>movies</li>
-    <li>tv</li>
-    <li>games</li>
-    <li>collectibles</li>
-    <li>videos</li>
-    <li>fans</li>
-    <li>news</li>
-    <li>shop</li>
-  </ul>
+  <div class="d-flex align-items-center justify-content-around">
+    <img src="../assets/img/dc-logo.png" alt="" />
+    <ul class="d-flex">
+      <li>characters</li>
+      <li>comics</li>
+      <li>movies</li>
+      <li>tv</li>
+      <li>games</li>
+      <li>collectibles</li>
+      <li>videos</li>
+      <li>fans</li>
+      <li>news</li>
+      <li>shop</li>
+    </ul>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 img {
   display: inline-block;
+}
+ul {
+  list-style-type: none;
+
+  li {
+    margin-left: 40px;
+    margin-top: 2px;
+    padding-bottom: 20px;
+    font-weight: 600;
+    border-bottom: 2px solid transparent;
+  }
+  :hover {
+    color: rgb(75, 75, 255);
+    border-bottom: 2px solid rgb(75, 75, 255);
+  }
 }
 </style>
